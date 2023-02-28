983cd9199f79b0692531c2a4272b5f27e42bab63
Plattenthaler <SME@dresden-elektronik.de>
2023-02-28 13:02:37 +0100

    working second axis

--- serialDataViewer.py
+++ serialDataViewer.py
@@ -37,6 +37,13 @@ def substring_after(s, delim):
 def substring_before(s, delim):
     return s.partition(delim)[0]
     
+#if nanmean is aplyed over a empty array (filled with "nan") a runtimewarning occures.
+def numpy_nan_mean(a):
+    if np.isnan(a[0]):
+        return np.NaN 
+    else:
+        return np.nanmean(a)
+    
 
 try:
     from version import Version
@@ -58,9 +65,12 @@ class Scope(object):
         self.tonausgabe = False #status der Tonausgabe
         self.string_to_strip_after = ''
         self.string_to_strip_bevore = 'placeholder'
-        self.ax = ax
+        self.ax_L = ax
+        self.ax_L.tick_params(axis='y', colors='blue')
+        self.ax_R = self.ax_L.twinx()
+        self.ax_R.tick_params(axis='y', colors='red')
         self.dt = dt
-        self.samples = 150 # wird durch submit uebernommen
+        self.samples = 500 # wird durch submit uebernommen
         self.npoints = self.samples #sind die waren samples!
         self.gleitt = self.npoints
         self.maxt = maxt
@@ -70,20 +80,30 @@ class Scope(object):
         self.dist_bins = 50 #Aufloesung der Verteilungsfunktion
         self.fft_time = 10 #in ms
         self.tdata = [0]
-        self.ydata = [0]
-        self.y_mittel = [0]
-        self.line_ymittel = Line2D(self.tdata, self.y_mittel, color="black")
-        self.line_yval = Line2D(self.tdata, self.ydata)
-        self.ax.add_line(self.line_ymittel)
-        self.ax.add_line(self.line_yval)
-        self.ax.set_ylim(-.1, self.samples)
-        self.ax.set_xlim(0, self.npoints)
+        self.ydata_L = [0]
+        self.ydata_R = [np.nan]
+        self.y_mittel_L = [0]
+        self.y_mittel_R = [np.nan]
+        self.line_ymittel_L = Line2D(self.tdata, self.y_mittel_L, color="blue", linestyle="--")
+        self.line_ymittel_R = Line2D(self.tdata, self.y_mittel_R, color="red" , linestyle="--")
+        self.line_yval_L = Line2D(self.tdata, self.ydata_L, color="blue")
+        self.line_yval_R = Line2D(self.tdata, self.ydata_R, color="red")
+        self.ax_L.add_line(self.line_ymittel_L)
+        self.ax_R.add_line(self.line_ymittel_R)
+        self.ax_L.add_line(self.line_yval_L)
+        self.ax_R.add_line(self.line_yval_R)
+        self.ax_L.set_ylim(-.1, self.samples)
+        self.ax_L.set_xlim(0, self.npoints)
         #erase outputdocuments
         #open("data-out.txt","w").close()
-        open("dist-out.txt","w").close()
-        open("fft-out.txt","w").close()
+        #open("dist-out.txt","w").close()
+        #open("fft-out.txt","w").close()
         
+        self.init_user_elements()
+        self.check_connection()
+        self.serial_connect()
         
+    def init_user_elements(self):    
         #die slider
         axcolor = 'lightgoldenrodyellow'
         self.skalierung_ymax = Slider(plt.axes([0.25, 0.08, 0.65, 0.02], facecolor=axcolor), 'Y Max', 10, 730.0 , valinit = 500)
@@ -109,6 +129,29 @@ class Scope(object):
         self.serial_status_text =       TextBox(plt.axes([0.605, 0.79,   0.345, 0.04]), '')
         #checkbox
         self.check = CheckButtons(plt.axes([0.72, 0.85, 0.11, 0.14]), ('Y-Auto', 'Data-out', 'Y-Log', 'Ton'), (True, False, False, False))
+               
+        #Button events
+        self.fft_button.on_clicked(self.fft_erstellen)
+        self.dist_button.on_clicked(self.dist_erstellen)
+        self.mess_toggle_button.on_clicked(self.mess_toggle_event)
+        self.sent_button.on_clicked(self.sent)
+        self.reset_button.on_clicked(self.reset)
+        self.single_autorange_button.on_clicked(self.single_autorange)
+        self.connect_button.on_clicked(self.serial_connect)
+        self.submit1_button.on_clicked(self.submit_samples)
+        
+        #Checkbox Events
+        self.check.on_clicked(self.funktion)
+          
+        #Text-Events
+        self.sent_box.on_submit(self.textupdate_sent)
+        self.fft_time_box.on_submit(self.textupdate_fft_time)
+        self.dist_bins_box.on_submit(self.textupdate_dist_bins)
+        self.samples_box.on_submit(self.textupdate_samples)
+        
+        #Slider-Events
+        self.skalierung_ymin.on_changed(self.range_manuell_anpassen)
+        self.skalierung_ymax.on_changed(self.range_manuell_anpassen)
         
     def mess_toggle_event(self, event):
         self.sendestatus = not self.sendestatus
@@ -157,47 +200,61 @@ class Scope(object):
         elif label == 'Y-Log':
             self.ylinlog = not self.ylinlog
             if self.ylinlog:
-                self.ax.set_yscale('symlog')
+                self.ax_L.set_yscale('symlog')
+                self.ax_R.set_yscale('symlog')
             else:
-                self.ax.set_yscale('linear')
-            self.single_autorange
+                self.ax_L.set_yscale('linear')
+                self.ax_R.set_yscale('linear')
+            self.single_autorange()
         elif label == 'Ton':
             self.tonausgabe = not self.tonausgabe
             if self.tonausgabe:
                 self.sinewave.play()
             else:
                 self.sinewave.stop()
-    def single_autorange(self, u):
-        self.ax.set_ylim(min(self.ydata)-(max(self.ydata)-min(self.ydata))/20,max(self.ydata)+(max(self.ydata)-min(self.ydata))/20)
     
-    def submit_samples(self, u):
+    def single_autorange(self, u=0):
+        ylim_min_l = min(self.ydata_L)-(max(self.ydata_L)-min(self.ydata_L))/20
+        ylim_max_l = max(self.ydata_L)+(max(self.ydata_L)-min(self.ydata_L))/20
+        if not(np.isnan(ylim_max_l)) and not(np.isnan(ylim_min_l)):
+            if ylim_max_l != ylim_min_l:
+                self.ax_L.set_ylim(ylim_min_l,ylim_max_l)
+        ylim_min_r = min(self.ydata_R)-(max(self.ydata_R)-min(self.ydata_R))/20
+        ylim_max_r = max(self.ydata_R)+(max(self.ydata_R)-min(self.ydata_R))/20
+        if not(np.isnan(ylim_max_r)) and not(np.isnan(ylim_min_r)):
+            if ylim_max_r != ylim_min_r:
+                self.ax_R.set_ylim(ylim_min_r,ylim_max_r)
+    
+    def submit_samples(self, u=0):
         self.npoints = self.samples # übernahme der werte von der eingabebox
         self.reset()
 
-    def range_manuell_anpassen(self, u):
-        self.ax.set_ylim(10 ** (self.skalierung_ymin.val/100), 10 ** (self.skalierung_ymax.val/100))
-        self.ax.figure.canvas.draw()
+    def range_manuell_anpassen(self, u=0):
+        self.ax_L.set_ylim(10 ** (self.skalierung_ymin.val/100), 10 ** (self.skalierung_ymax.val/100))
+        self.ax_R.set_ylim(10 ** (self.skalierung_ymin.val/100), 10 ** (self.skalierung_ymax.val/100))
+        self.ax_L.figure.canvas.draw()
 
-    def range_anpassen(self, u):
+    def range_anpassen(self, u=0):
         if self.autoadjust == True :
             self.single_autorange(self)
         else:
-            #self.ax.set_ylim(10 ** (self.skalierung_ymin.val/100), 10 ** (self.skalierung_ymax.val/100))
+            #self.ax_L.set_ylim(10 ** (self.skalierung_ymin.val/100), 10 ** (self.skalierung_ymax.val/100))
             pass
-        self.ax.figure.canvas.draw()
+        self.ax_L.figure.canvas.draw()
+        return self.line_yval_L, self.line_yval_R, self.line_ymittel_L, self.line_ymittel_R
     
     def fft_erstellen(self, g=3):
         #print(self.sent_box_fft_time.val)
         dt = int(self.fft_time)/1000 #samplingtime in sekunden
         fa = 1.0/dt # scan frequency
-        Y=np.fft.fft(np.hanning(len(self.ydata))*self.ydata)
+        Y=np.fft.fft(np.hanning(len(self.ydata_L))*self.ydata_L)
         N = len(Y)
         X = np.linspace(0, fa/2, N, endpoint=True)
         #x = np.linspace(0, 2*np.pi, 400)
         #y = np.sin(x**2)
         fig2, ax2 = plt.subplots()
         #fig2.subplots_adjust(bottom=0.15)
-        ax2.set_title(r'$\mathrm{FFT:}\ Freq=%.2f Hz,\ samples=%.0f,\ T=%.3f$ ms' %(fa, len(self.ydata), dt*1000))#"FFT-Plot Samplingfreq: " + str(fa) + "Hz, Periodendauer: " + str(dt*1000) + "ms") 
+        ax2.set_title(r'$\mathrm{FFT:}\ Freq=%.2f Hz,\ samples=%.0f,\ T=%.3f$ ms' %(fa, len(self.ydata_L), dt*1000))#"FFT-Plot Samplingfreq: " + str(fa) + "Hz, Periodendauer: " + str(dt*1000) + "ms") 
         ax2.plot(X,np.abs(Y))
         ax2.set_xscale('log')
         ax2.set_yscale('log')
@@ -208,10 +265,10 @@ class Scope(object):
     
     def dist_erstellen(self, g=3) :
         fig_verteilung, ax_verteilung = plt.subplots()
-        sigma = np.std(self.ydata)
-        #varianz = np.var(self.ydata)
-        mu = np.mean(self.ydata)
-        n, bins, patches = plt.hist(self.ydata, bins=self.dist_bins, density=True) #density -> normed distribution
+        sigma = np.std(self.ydata_L)
+        #varianz = np.var(self.ydata_L)
+        mu = np.nanmean(self.ydata_L)
+        n, bins, patches = plt.hist(self.ydata_L, bins=self.dist_bins, density=True) #density -> normed distribution
         #y = mlab.normpdf( bins, mu, sigma) #old, not working
         gausverteilung =  1/(sigma * np.sqrt(2 * np.pi)) * np.exp( - (bins - mu)**2 / (2 * sigma**2) ) 
         l = plt.plot(bins, gausverteilung, 'r--', linewidth=2)
@@ -223,7 +280,7 @@ class Scope(object):
         #f.close()
         
         #ax_verteilung.set_title("Verteilung mit sigm: " + str(standartabweichung) + " Varianz: " + str(varianz) + " und Mittelwert: " + str(mittelwert)) 
-        ax_verteilung.set_title(r'$\mathrm{Histogram}\ \mu=%.1f,\ \sigma=%.3f$, Samples=%.0f Bins=%.0f' %(mu, sigma, len(self.ydata), self.dist_bins))
+        ax_verteilung.set_title(r'$\mathrm{Histogram}\ \mu=%.1f,\ \sigma=%.3f$, Samples=%.0f Bins=%.0f' %(mu, sigma, len(self.ydata_L), self.dist_bins))
         
         #plt.axis([0, 10000, 0, 10])
         plt.grid(True)
@@ -267,18 +324,21 @@ class Scope(object):
             self.serial_status_text.set_val(str(self.serialPort.port + ", " + str(self.serialPort.baudrate) + ": Disconnected"))
         
     def reset(self, g=0):
-        mittelwert=np.mean(self.ydata)
-        del self.tdata [:]
-        del self.ydata [:]
+
         #2 Daten anhaengen, damit auto x-einstellung funktioniert
         self.tdata.append(0)
-        self.ydata.append(mittelwert)
+        self.ydata_L.append(np.nanmean(self.ydata_L))
+        self.ydata_R.append(np.nanmean(self.ydata_R))
+        del self.tdata[:-1  ]
+        del self.ydata_L [:-1]
+        del self.ydata_R [:-1]
         self.npoints = self.samples
-        self.ax.set_xlim(0, self.npoints)
+        self.ax_L.set_xlim(0, self.npoints)
+        self.ax_R.set_xlim(0, self.npoints)
         self.gleitt=self.npoints
-        self.ax.figure.canvas.draw()
+        self.ax_L.figure.canvas.draw()
     
-    def update(self, i):
+    def update(self, i=0):
         i = 0;
         in_waiting = 0
         data_available = 0
@@ -287,9 +347,11 @@ class Scope(object):
         except Exception as e:
             #print(e)
             self.check_connection()
-            return self.line_yval, self.line_ymittel
+            return self.line_yval_L, self.line_yval_R, self.line_ymittel_L, self.line_ymittel_R
         read_data_len = 1000
-        ydata = np.empty(read_data_len+1)
+        ydata_L = np.empty(read_data_len+1)
+        ydata_R = np.empty(read_data_len+1)
+        
         #tdata[350]  
         #tdata[0] = self.tdata[-1] + 1
         while (in_waiting != 0) & (i<read_data_len):         
@@ -313,16 +375,22 @@ class Scope(object):
                 print(e)
             if inputline != "": #weil bestätigung leerer string ist
                 try:
-                    wert=float(inputline) # war int, hoffe das macht keine probleme
-                    ydata[i] = wert
+                    if(inputline.find(";")==-1):  # If just one Value   
+                        wert_L=float(inputline) # war int, hoffe das macht keine probleme
+                        wert_R=float('nan')
+                    else:
+                        wert_L=float(inputline[:inputline.find(";")])
+                        wert_R=float(inputline[inputline.find(";")+1:])
+                        
+                    ydata_L[i] = wert_L
+                    ydata_R[i] = wert_R
                     data_available +=1
                     #self.tdata.append(self.tdata[-1] + 1)
-                    if self.datenausgabe == True :
-                        print(wert)
-                        print(wert, file=open("data-out.txt","a"))
                     if self.tonausgabe== True :
-                        self.sinewave.set_frequency(wert*1.5+220)
+                        self.sinewave.set_frequency(wert_L*1.5+220)
                 except Exception as e:
+                    ydata_L[i] = np.nan
+                    ydata_R[i] = np.nan
                     print(e)
             else:
                 #print("leerer string, befehl uebertragen!")
@@ -332,34 +400,47 @@ class Scope(object):
         if(data_available > 0):
             if (data_available>read_data_len*0.7):
                 print("too much data", data_available)
-            if len(self.ydata)==1: #um initialen wert zu entfernen
-                self.ydata[0]=ydata[0]        
+            if len(self.ydata_L)==1: #um initialen wert zu entfernen
+                self.ydata_L[0]=ydata_L[0]
+                self.ydata_R[0]=ydata_R[0]        
+
+            if self.datenausgabe == True :
+                ausgabetext = ""
+                for ausg_i in range(i):
+                    ausgabetext += str(ydata_L[ausg_i])+ "\t"+str(ydata_R[ausg_i])+"\r\n"
+                print(ausgabetext[:ausgabetext.find("\r")])
+                print(ausgabetext, file=open("data-out.txt","a"))
             
-            self.ydata.extend(ydata[0:i].tolist())
+            self.ydata_L.extend(ydata_L[0:i].tolist())
+            self.ydata_R.extend(ydata_R[0:i].tolist())
             self.tdata.extend(np.arange(self.tdata[-1]+1,self.tdata[-1]+i+1).tolist())
                
-            self.ydata     = self.ydata   [-1 * self.npoints:]   # Auf bereich anpassen
+            self.ydata_L     = self.ydata_L   [-1 * self.npoints:]   # Auf bereich anpassen
+            self.ydata_R     = self.ydata_R   [-1 * self.npoints:]   # Auf bereich anpassen
             self.tdata     = self.tdata   [-1 * self.npoints:]   # Auf bereich anpassen
             
         if self.tdata[-1] > self.gleitt:  # reset the arrays
             self.gleitt = self.tdata[-1] + self.npoints*0.1
-            self.ax.set_xlim(self.tdata[0]+ self.npoints*0.1, self.tdata[-1] + self.npoints*0.1)
+            self.ax_L.set_xlim(self.tdata[0]+ self.npoints*0.1, self.tdata[-1] + self.npoints*0.1)
+            self.ax_R.set_xlim(self.tdata[0]+ self.npoints*0.1, self.tdata[-1] + self.npoints*0.1)
             if self.autoadjust == True :
                 self.range_anpassen(self)
             else:
-                #self.ax.set_ylim(10 ** (self.skalierung_ymin.val/100), 10 ** (self.skalierung_ymax.val/100))
+                #self.ax_L.set_ylim(10 ** (self.skalierung_ymin.val/100), 10 ** (self.skalierung_ymax.val/100))
                 pass
-            self.ax.figure.canvas.draw()
-        if len(self.tdata)==10: #damit wertebreich zu beginn sinvoll angepasst wird
+            self.ax_L.figure.canvas.draw()
+        if len(self.tdata)<self.samples and len(self.tdata)%int(self.samples/10): #for the initial filling
             if self.autoadjust == True :
                 self.range_anpassen(self)
-                self.ax.figure.canvas.draw()
+                self.ax_L.figure.canvas.draw()
         
         #Mittelwertbildung
-        self.line_ymittel.set_data(self.tdata, [np.mean(self.ydata)])
-        self.line_yval.set_data(self.tdata, self.ydata)
+        self.line_ymittel_L.set_data(self.tdata, [np.nanmean(self.ydata_L)])
+        self.line_ymittel_R.set_data(self.tdata, [numpy_nan_mean(self.ydata_R)])
+        self.line_yval_L.set_data(self.tdata, self.ydata_L)
+        self.line_yval_R.set_data(self.tdata, self.ydata_R)
         
-        return self.line_yval, self.line_ymittel,
+        return self.line_yval_L, self.line_yval_R, self.line_ymittel_L, self.line_ymittel_R
 
 # Fixing random state for reproducibility
 np.random.seed(19680801)
@@ -387,33 +468,9 @@ def main(args = None):
     fig.subplots_adjust(bottom=0.15, top=0.78)
     
     scope = Scope(ax, 10, 0.01, serial_port_connection)
-    scope.check_connection()
-    scope.serial_connect()
-
-    #Button events
-    scope.fft_button.on_clicked(scope.fft_erstellen)
-    scope.dist_button.on_clicked(scope.dist_erstellen)
-    scope.mess_toggle_button.on_clicked(scope.mess_toggle_event)
-    scope.sent_button.on_clicked(scope.sent)
-    scope.reset_button.on_clicked(scope.reset)
-    scope.single_autorange_button.on_clicked(scope.single_autorange)
-    scope.connect_button.on_clicked(scope.serial_connect)
-    scope.submit1_button.on_clicked(scope.submit_samples)
-    
-    #Checkbox Events
-    scope.check.on_clicked(scope.funktion)
-      
-    #Text-Events
-    scope.sent_box.on_submit(scope.textupdate_sent)
-    scope.fft_time_box.on_submit(scope.textupdate_fft_time)
-    scope.dist_bins_box.on_submit(scope.textupdate_dist_bins)
-    scope.samples_box.on_submit(scope.textupdate_samples)
-    
-    #Slider-Events
-    scope.skalierung_ymin.on_changed(scope.range_manuell_anpassen)
-    scope.skalierung_ymax.on_changed(scope.range_manuell_anpassen)
     
     ani = animation.FuncAnimation(fig, scope.update, interval=50, blit=True, save_count=500)
+    #ani2 = animation.FuncAnimation(fig, scope.range_anpassen, interval=1000, blit=True, save_count=500)
     scope.textupdate_samples
     g1 = ax.grid(visible=True, which='major', color='k', linestyle='-', linewidth=0.5)
     g2 = ax.grid(visible=True, which='minor', color='k', linestyle='-', linewidth=0.2)
